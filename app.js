64fae4b284f5d82414bcca7d7ea2953c576bd18e
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-07-03 03:04:43 +0530

    Copied server file from ISRO.

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const planetsRouter = require('./src/routes/planets/planets.router')
+const cors = require('cors')
+const path = require('path')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+)
+app.use(morgan('combined'))
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public'))) //for serving static files on server.
+
+app.use(planetsRouter)
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+module.exports = app
